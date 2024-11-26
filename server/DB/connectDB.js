def0113c36170a1e10c57ad7ed44ccfab556a3e5
Aditya-Bajpayee007 <adityabajpayee7@gmail.com>
2024-11-27 01:08:32 +0530

    mongoose

--- server/DB/connectDB.js
+++ server/DB/connectDB.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 async function connectDB() {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_DB_URI);
+    const connection = await mongoose.connect(process.env.MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connection Established");
   } catch (error) {
     console.log(error);
